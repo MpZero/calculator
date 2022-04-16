cd8a23a1ed555476a8dd6507a39b0911199f2b89
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-16 20:01:01 -0300

    add functions for all of the basic math operators

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+let num1 = 5
+let num2 = 5
+function add() {
+return num1 + num2
+}
+
+function subtract() {
+  return num1 - num2
+}
+
+function multiply() {
+  return num1 * num2
+}
+
+function divide() {
+  return num1 / num2
+}
+
