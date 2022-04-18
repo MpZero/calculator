37a14b60b9e1e72f9b96566919a3b71a7bfad8e0
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-18 15:46:33 -0300

    add events to all buttons

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-let num1 = 5
-let num2 = 5
+let num1 = 5;
+let num2 = 5;
+let displayValue = document.getElementById("displayNum");
+const buttons = document.querySelectorAll("button");
 
 function add(a, b) {
-return a + b
+return a + b;
 }
 
 function subtract(a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply(a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide(a, b) {
-  return a / b
+  return a / b;
 }
 
 function operate() {
-  return add(num1, num2)
+  //when user presses "=" key, then run operate and pass the result to the display
+  return displayValue.textContent = button(num1, num2);
+
   //change "add" for button later
 }
+
+// function eventListeners(){
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      console.log("h")
+    })
+  });
+// }
+
+ //create function that stores the value of the numbers
+ //pass that value to the display
+
+ //create function that adds event listeners to all buttons
+ //when button is clicked call its value
