5642e76077b3bfcd1e1f9514e1e0c4bdb13591ed
gonzalo <gonzalo.a.gomez22@gmail.com>
2022-04-24 15:52:54 -0300

    add a clear function, display the stored values(numbers and operators) correctly and do the desired operation at least 1 time

--- main.js
+++ main.js
@@ -1,41 +1,101 @@
-let num1 = 5;
-let num2 = 5;
-let displayValue = document.getElementById("displayNum");
-const buttons = document.querySelectorAll("button");
+let displayValue = document.getElementById("displayVal");
+const btnMisc = document.querySelectorAll(".btn")
+const btnNum = document.querySelectorAll(".btn-num");
+const btnOp = document.querySelectorAll(".btn-op");
+let num1 = ""
+let num2 = ""
+let op = ""
+let result = ""
 
-function add(a, b) {
-return a + b;
+function add() {
+ result =parseInt(num1) + parseInt(num2);
+ displayValue.textContent = result
 }
 
-function subtract(a, b) {
-  return a - b;
+function subtract() {
+  console.log(parseInt(num1) - parseInt(num2));
 }
 
-function multiply(a, b) {
-  return a * b;
+function multiply() {
+  console.log(parseInt(num1) * parseInt(num2));
 }
 
-function divide(a, b) {
-  return a / b;
+function divide() {
+  console.log(parseInt(num1) / parseInt(num2));
 }
 
 function operate() {
-  //when user presses "=" key, then run operate and pass the result to the display
-  return displayValue.textContent = button(num1, num2);
-
-  //change "add" for button later
+  if (op == "/") {
+    return divide()
+  }
+  if (op == "*") {
+    return multiply()
+  }
+  if (op == "+") {
+    return add()
+  }
+  if (op == "-") {
+    return subtract()
+  }
 }
 
-// function eventListeners(){
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      console.log("h")
-    })
+btnNum.forEach((button) => {
+  button.addEventListener("click", () => {
+    if ( op === "") {
+      num1 += button.textContent;
+      displayValue.textContent = num1;
+      // console.log(num1)
+    } else {
+      num2 += button.textContent;
+      displayValue.textContent = num1 + op + num2
+      // console.log(num2)
+    }
+    });
   });
+
+btnOp.forEach((button) => {
+  button.addEventListener("click", () => {
+      // console.log(op = button.textContent);
+      op = button.textContent;
+      displayValue.textContent = num1 + button.textContent;
+        })
+});
+
+btnMisc.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let misc = e.target.textContent
+     if (misc == "=") {
+      return operate()
+    } else if (misc == "CLEAR") {
+      return clear()
+    } else if (misc == "DELETE") {
+      return deleteChar()
+    }
+  });
+});
+
+function clear() {
+  num1 = ""
+  num2 = ""
+  op = ""
+  displayValue.textContent = ""
+}
+
+// function switchNum() {
+  
 // }
 
+// function deleteChar() {
+
+// }
  //create function that stores the value of the numbers
  //pass that value to the display
 
  //create function that adds event listeners to all buttons
  //when button is clicked call its value
+
+ //el usuario presiona un boton
+ //el valor del boton se guarda en una variable y se visualiza en la pantalla
+ //cuando el usuario presiona un operador, este se guarda
+
+ //
